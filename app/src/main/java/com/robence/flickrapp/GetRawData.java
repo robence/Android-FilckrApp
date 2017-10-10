146093620049c9803f8d1891a370fa358b3e8d19
Rozsa Bence <robence@gmail.com>
2017-10-10 16:15:28 +0200

    Data downloder added to main

--- app/src/main/java/com/robence/flickrapp/GetRawData.java
+++ app/src/main/java/com/robence/flickrapp/GetRawData.java
@@ -27,7 +27,7 @@ class GetRawData extends AsyncTask<String, Void, String> {
 
     @Override
     protected void onPostExecute(String s) {
-        super.onPostExecute(s);
+        Log.d(TAG, "onPostExecute: parameter = " + s);
     }
 
     @Override
@@ -54,13 +54,36 @@ class GetRawData extends AsyncTask<String, Void, String> {
 
             reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
+//            String line;
+//            while (null != (line = reader.readLine())) {
+            for(String line = reader.readLine(); line != null; line = reader.readLine()) {
+                result.append(line).append("\n");
+            }
+
+            mDownloadStatus = DownloadStatus.OK;
+            return result.toString();
+
+
         } catch (MalformedURLException e) {
             Log.e(TAG, "doInBackground: Invalid URL " + e.getMessage( ));
         } catch (IOException e) {
             Log.e(TAG, "doInBackground: IO Exception reading data: " + e.getMessage() );
         } catch (SecurityException e) {
             Log.e(TAG, "doInBackground: Security Exception. Needs permission? " + e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    Log.e(TAG, "doInBackground: Error closing stream " + e.getMessage() );
+                }
+            }
         }
+
+        mDownloadStatus = DownloadStatus.FAILED_OR_EMPTY;
         return null;
     }
 }

0c0e28b4910c1e69f1aecfab899e636f2b690c56
Rozsa Bence <robence@gmail.com>
2017-10-12 18:34:41 +0200

    Search activity

--- app/src/main/java/com/robence/flickrapp/SearchActivity.java
+++ app/src/main/java/com/robence/flickrapp/SearchActivity.java
@@ -1,20 +1,44 @@
 package com.robence.flickrapp;
 
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.SearchView;
 
 public class SearchActivity extends BaseActivity {
 
+    private static final String TAG = "SearchActivity";
+    private SearchView mSearchView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: starts");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
         activateToolbar(true);
+        Log.d(TAG, "onCreate: ends");
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d(TAG, "onCreateOptionsMenu: starts");
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        mSearchView = (SearchView) menu.findItem(R.id.app_bar_search).getActionView();
+        SearchableInfo searchableInfo = searchManager.getSearchableInfo(getComponentName());
+        mSearchView.setSearchableInfo(searchableInfo);
+        Log.d(TAG, "onCreateOptionsMenu: " + getComponentName().toString());
+        Log.d(TAG, "onCreateOptionsMenu: hint is " + mSearchView.getQueryHint());
+        Log.d(TAG, "onCreateOptionsMenu: searchable info is " + searchableInfo.toString());
+
+        mSearchView.setIconified(true);
+
+        Log.d(TAG, "onCreateOptionsMenu: returned " + true);
+        return true;
+    }
 }

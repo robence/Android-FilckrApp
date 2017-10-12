bfad3d096df4bc2397cb9d7e8737d80e32ebb70f
Rozsa Bence <robence@gmail.com>
2017-10-12 14:33:32 +0200

    ClickListener and Intercepting Events

--- app/src/main/java/com/robence/flickrapp/FlickrRecyclerViewAdapter.java
+++ app/src/main/java/com/robence/flickrapp/FlickrRecyclerViewAdapter.java
@@ -48,7 +48,6 @@ class FlickrRecyclerViewAdapter extends RecyclerView.Adapter<FlickrRecyclerViewA
 
     @Override
     public int getItemCount() {
-        Log.d(TAG, "getItemCount: called");
         return ((mPhotoList!= null) && (mPhotoList.size() != 0) ? mPhotoList.size() : 0);
     }
 
